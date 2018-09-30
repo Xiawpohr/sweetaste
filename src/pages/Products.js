32ee62ca57299eab5676ed7248afa6f178b84e31
XiawPohr <paradise121329@gmail.com>
2018-09-30 18:32:21 +0800

    refactor product page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Mobile from '../components/Mobile.js'
 import Desktop from '../components/Desktop.js'
+import Quote from '../components/Quote.js'
 import CategoryList from '../components/CategoryList.js'
 import ProductList from '../components/ProductList.js'
 import Pagination from '../components/Pagination.js'
@@ -8,7 +9,6 @@ import Container from '../styles/Container.js'
 import Flex from '../styles/Flex.js'
 import Box from '../styles/Box.js'
 import Background from '../styles/Background.js'
-import Text from '../styles/Text.js'
 import havingDessert from '../images/having-dessert.jpg'
 
 const Products = () => (
@@ -16,9 +16,9 @@ const Products = () => (
     <Mobile>
       <Background height={277} backgroundImage={`url(${havingDessert})`}>
         <Flex justifyContent='flex-end'>
-          <Text mt={3} mr={3} fontSize={22} color='darkGreen' mode='vertical-rl' letterSpacing={6.5}>
-            想吃甜點——<br /><span style={{ marginTop: 11 }}>是不需要理由的。</span>
-          </Text>
+          <Box mt={3} mr={3}>
+            <Quote firstLine='想吃甜點' secondLine='是不需要理由的。' fontSize={22} letterSpacing={6} />
+          </Box>
         </Flex>
       </Background>
       <CategoryList />
@@ -33,9 +33,9 @@ const Products = () => (
       <Container>
         <Background height={496} backgroundImage={`url(${havingDessert})`}>
           <Flex justifyContent='flex-end'>
-            <Text mt={3} mr={3} fontSize={3} color='darkGreen' mode='vertical-rl' letterSpacing={6.5}>
-              想吃甜點——<br /><span style={{ marginTop: 11 }}>是不需要理由的。</span>
-            </Text>
+            <Box mt={3} mr={3}>
+              <Quote firstLine='想吃甜點' secondLine='是不需要理由的。' />
+            </Box>
           </Flex>
         </Background>
         <Flex my={5}>
